4f635c3fc547ca356077e3597e9bcf999654b85a
Pico143 <pico19942@gmail.com>
2018-05-02 15:24:45 +0200

    implemented shuffling of image classes

--- static/game.js
+++ static/game.js
@@ -1,11 +1,30 @@
-var fontAwesome = [];
-fontAwesome.push("f0f9","f103","f100","f101","f102","f107","f104","f105","f106","f179","f187");
-fontAwesome.push("f13d","f1fe","f0ab","f0a8","f01a","f190","f18e","f01b","f0a9","f0aa","f063");
-fontAwesome.push("f17b","f060","f061","f062","f047","f0b2","f07e","f07d","f069","f1fa");
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var fontAwesome = ["f0f9", "f103", "f100", "f101", "f102", "f107", "f104", "f105", "f106", "f179", "f187",
+    "f13d", "f1fe", "f0ab", "f0a8", "f01a", "f190", "f18e", "f01b", "f0a9", "f0aa", "f063",
+    "f17b", "f060", "f061", "f062", "f047", "f0b2", "f07e", "f07d", "f069", "f1fa"];
+
 
 let game = {
     buttons: document.querySelectorAll('.card'),
-    evenMove : true, // if false after move, wait for second card, if true check if cards are the same
+    evenMove: true, // if false after move, wait for second card, if true check if cards are the same
     turnCard: function (event) {
         game.evenMove = !game.evenMove;
         if (game.evenMove === true) {
@@ -23,7 +42,7 @@ let game = {
     },
     getImagesArray: function () {
         let images = [];
-        for (let i = 0; i<(game.buttons.length/2); i++) {
+        for (let i = 0; i < (game.buttons.length / 2); i++) {
             let randomIndex = Math.floor(Math.random() * fontAwesome.length);
             if (images.indexOf(fontAwesome[randomIndex]) === -1) {
                 images.push(fontAwesome[randomIndex]);
@@ -33,6 +52,8 @@ let game = {
                 continue;
             }
         }
+        images = images.concat(images);
+        shuffle(images)
         return images;
     }
 }
